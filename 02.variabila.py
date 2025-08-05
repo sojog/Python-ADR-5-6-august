200601c3bb683892d0337351ace4a5b1fcac68db
Silviu Ojog <sil.ojog@gmail.com>
2025-08-05 10:22:01 +0300

    Tipuri de date

---
+++ 02.variabila.py
@@ -0,0 +1,37 @@
+
+x = 10
+print("x=",x)
+
+y = x + 3
+print("y=",y)
+
+print("x=",x)
+
+
+temperatura = 22
+print(temperatura)
+
+
+# snake_case - preferata
+temperatura_camerei = 22
+print(temperatura_camerei)
+
+
+# camelCase - nu este preferat (Doar pentru OOP - Programare Orientata Obiect)
+temperaturaCamerei = 28
+print(temperaturaCamerei)
+
+# nu trebuie sa inceapa cu o cifra, dar poate contine cifre 
+om1 = "George"
+print(om1)
+
+# nu pot fi cuvinte cheie (ex: if)
+IF = 400
+print(IF)
+
+# If - else -> keywords, cuvinte cheie
+# Conditie 
+if 10 > 2:
+    print("Mai mare")
+else:
+    print("Mai mic")
---
+++ 03.tipuri_de_date.py
@@ -0,0 +1,13 @@
+
+# Primitive - boolean, integer, floating point, string
+x = True # sau False
+print(x, type(x))
+
+x = -1321
+print(x, type(x))
+
+x = 21.32
+print(x, type(x))
+
+x = "salut"
+print(x, type(x))
